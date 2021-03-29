6a23ffe55226d5f6a977369db01e84ce3dabb07e
AmirAliShokri <amirali.shokri12@gmail.com>
2021-03-29 21:56:17 +0430

    Base Fix Trainer For Init Train

--- NCR.AI.Trainer/Program.cs
+++ NCR.AI.Trainer/Program.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using Common;
 using Microsoft.ML;
 using Microsoft.ML.AutoML;
 using NCR.AI.Model;
+using SHA.BeautifulConsoleColor.Core.Class;
+using SHA.BeautifulConsoleColor.Core.Extension;
+using SHA.BeautifulConsoleColor.Core.Model;
 
 namespace NCR.AI.Trainer
 {
@@ -11,25 +15,63 @@ namespace NCR.AI.Trainer
     {
         static void Main(string[] args)
         {
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine("News Classification Trainer Started");
+            BCCConsole.Write(BCCConsoleColor.Blue,false, "News Classification Trainer Started");
 
-            FindTheBestModel();
+            //FindTheBestModel();
+            TrainModel();
+        }
 
-
-            Console.ResetColor();
+        private static void TrainModel()
+        {
+            BCCConsole.Write(BCCConsoleColor.Gray,false,"Trainer Base Is Started ...");
+            var mlContext = new MLContext(0);
+            string trainDataPath = "Data\\uci-news-aggregator.csv";
+            string trainCachePath = @"Cache\";
+            var trainDataView = mlContext.Data.LoadFromTextFile<ModelInput>(
+                trainDataPath,
+                hasHeader: true,
+                separatorChar: ',',
+                allowQuoting: true
+            );
+            var preProcessingPipeline = mlContext.Transforms
+                .Conversion.MapValueToKey("Label","Category")
+                .Append(mlContext.Transforms.Text.FeaturizeText(inputColumnName: "Title",outputColumnName: "Features"))
+                .Append(mlContext.Transforms.NormalizeMinMax("Features"))
+                .AppendCacheCheckpoint(mlContext);
+            var trainer = mlContext.MulticlassClassification.Trainers
+                .OneVersusAll(mlContext.BinaryClassification.Trainers.AveragedPerceptron());
+            var trainingPipeline = preProcessingPipeline
+                .Append(trainer)
+                .Append(
+                    mlContext.Transforms.Conversion.MapKeyToValue("PredictedLabel")
+                    );
+            BCCConsole.Write(BCCConsoleColor.Cyan,false,"Cross Validation Is Starting . . .");
+            var cvResult = mlContext.MulticlassClassification
+                .CrossValidate(trainDataView, trainingPipeline);
+            BCCConsole.Write(BCCConsoleColor.DarkGreen,false,"\n",
+                "Cross Validation Result Metrics",
+                "-----------------------------------");
+            var micA = cvResult.Average(m => m.Metrics.MicroAccuracy).ToString("0.###").DarkGreen();
+            var macA = cvResult.Average(m => m.Metrics.MacroAccuracy).ToString("0.###").DarkGreen();
+            var logA = cvResult.Average(m => m.Metrics.LogLossReduction).ToString("0.###").DarkGreen();
+            BCCConsole.Write(BCCConsoleColor.DarkGreen,false, "-----------------------------------");
+            var finalModel = trainingPipeline.Fit(trainDataView);
+            var modelPath = "Model\\NewsClassificationModel.zip";
+            if (!Directory.Exists("Model\\"))
+            {
+                Directory.CreateDirectory("Model\\");
+            }
+            BCCConsole.Write(BCCConsoleColor.Yellow,false,"Saving Model . . .");
+            mlContext.Model.Save(finalModel,trainDataView.Schema,modelPath);
+            BCCConsole.Write(BCCConsoleColor.Green,false,"Saved !");
         }
 
         private static void FindTheBestModel()
         {
-            Console.ForegroundColor = ConsoleColor.DarkGreen;
-            Console.WriteLine("\nFinding the Best Model Using AutoML");
-
+            BCCConsole.Write(BCCConsoleColor.DarkGreen,false, "\nFinding the Best Model Using AutoML");
             var mlContext = new MLContext(0);
-
             string trainDataPath = @"Data\uci-news-aggregator.csv";
             string trainCachePath = @"Cache\";
-
             var trainDataView = mlContext.Data.LoadFromTextFile<ModelInput>(
                 trainDataPath,
                 hasHeader:true,
@@ -47,25 +89,16 @@ namespace NCR.AI.Trainer
                 OptimizingMetric = MulticlassClassificationMetric.MicroAccuracy,
                 CacheDirectory = new DirectoryInfo(trainCachePath)
             };
-
             var experiment = mlContext.Auto().CreateMulticlassClassificationExperiment(experimentSetting);
             var experimentResult = experiment.Execute(
                 trainData:mappedInputData,
                 labelColumnName:"Category",
                 progressHandler:new MulticlassExperimentProgressHandler()
                 );
-
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine("Metrics From Best Run ... ");
-
+            BCCConsole.Write(BCCConsoleColor.Yellow,false, "Metrics From Best Run ... ");
             var metrics = experimentResult.BestRun.ValidationMetrics;
-
-            Console.ForegroundColor = ConsoleColor.DarkBlue;
-            Console.WriteLine($"Metric Micro Accuracy : {metrics.MicroAccuracy:0.##}");
-            Console.WriteLine($"Metric Micro Accuracy : {metrics.MicroAccuracy:0.##}");
-
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Success !");
+            BCCConsole.Write(BCCConsoleColor.DarkGreen,false, $"Metric Micro Accuracy : {metrics.MicroAccuracy:0.##}"); 
+            BCCConsole.Write(BCCConsoleColor.Green,false,"Success !");
         }
     }
 }
